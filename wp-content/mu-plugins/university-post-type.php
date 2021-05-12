2c63b3628b0150afd5562aa4ba0aaab2dbdf8f27
nikesuputra <nike.suputra@gmail.com>
2021-05-12 16:05:05 +0800

    customize Event post type

--- wp-content/mu-plugins/university-post-type.php
+++ wp-content/mu-plugins/university-post-type.php
@@ -5,7 +5,10 @@ function university_post_types() {
       'public' => true,
       'labels' => array(
         'name' => 'Events',
-        'add_new_item' => 'Add New Event'
+        'add_new_item' => 'Add New Event',
+        'edit_item' => 'Edit Event',
+        'all_items' => 'All Events',
+        'singular_name' => 'Event'
       ),
       'menu_icon' => 'dashicons-calendar'
     ));
